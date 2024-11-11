66f5bcbaa99dbb5a92b03b745dd5642da5528719
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-10 19:01:47 -0600

    M - 7:01 PM - added the errorhandler to the app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 starts the flask server the the needed information
 """
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -15,6 +15,11 @@ app.register_blueprint(app_views)
 def close_storage(exception):
     """closes storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
