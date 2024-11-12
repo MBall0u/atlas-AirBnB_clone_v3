fce85cd48c0699111035a62b210fdc48c7419ea7
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-12 16:04:36 -0600

    M - 4:04 PM - made changes for pep8 and documentation

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""module for Cities that interacts with the City model in the database"""
 from api.v1.views import app_views
 from flask import jsonify, request, abort
 from models import storage
