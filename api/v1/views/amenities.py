510f36d4b6f037d96d45eef3355aff71268c36be
Zytronium <dastelljes@outlook.com>
2024-11-12 18:52:57 -0600

    added some documentation and created amenities.py

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""module for Amenities that interacts with the Amenity model in the database"""
+from flask import jsonify, request, abort
+from models.amenity import Amenity
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/amenities', methods=['GET'])
+def get_amenities():
+    """Gets the list of all Amenity objects"""
+    all_amenities = storage.all(Amenity).values()
+    amenities_list = [amenity.to_dict() for amenity in all_amenities]
+
+    return jsonify(amenities_list)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+def get_amenity(amenity_id):
+    """Gets an Amenity object with the given ID"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    return jsonify(amenity.to_dict())
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+def delete_amenity(amenity_id):
+    """Deletes an Amenity object with the given ID"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    storage.delete(amenity)
+    storage.save()
+
+    return jsonify({}), 200
+
+
+@app_views.route('/amenities', methods=['POST'])
+def create_amenity():
+    """Creates a new Amenity object"""
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+
+    request_data = request.get_json()
+    if 'name' not in request_data:
+        abort(400, description="Missing name")
+
+    new_amenity = Amenity(**request_data)
+    storage.new(new_amenity)
+    storage.save()
+
+    return jsonify(new_amenity.to_dict()), 201
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+def update_amenity(amenity_id):
+    """Updates an existing Amenity object with the given ID"""
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+
+    if not request.is_json:
+        abort(400, description="Not a JSON")
+
+    request_data = request.get_json()
+    ignore_keys = ['id', 'created_at', 'updated_at']
+
+    for key, value in request_data.items():
+        if key not in ignore_keys:
+            setattr(amenity, key, value)
+
+    storage.save()
+
+    return jsonify(amenity.to_dict()), 200
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
+    """Gets a list of all State objects"""
     all_states = storage.all(State).values()
     states_list = [state.to_dict() for state in all_states]
 
@@ -16,6 +17,7 @@ def get_states():
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
+    """Gets a single State object with the given ID"""
     state = storage.get(State, state_id)
 
     if not state:
@@ -28,6 +30,7 @@ def get_state(state_id):
     '/states/<state_id>', methods=['DELETE'], strict_slashes=False
     )
 def delete_state(state_id):
+    """Deletes a single State object with the given ID"""
     state = storage.get(State, state_id)
 
     if not state:
@@ -41,6 +44,7 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
+    """Creates a new State object"""
     if not request.is_json:
         abort(400, description="Not a JSON")
 
@@ -58,6 +62,7 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
+    """Updates a State object with the given ID"""
     if not request.is_json:
         abort(400, description="Not a JSON")
 
@@ -66,10 +71,10 @@ def update_state(state_id):
     if not state:
         abort(404)
 
-    req_data = request.get_json()
+    request_data = request.get_json()
     ignore_keys = ['id', 'created_at', 'updated_at']
 
-    for key, value in req_data.items():
+    for key, value in request_data.items():
         if key not in ignore_keys:
             setattr(state, key, value)
 
