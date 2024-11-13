0350a08e63b27d1da9c1b467686b853dce35ca01
Zytronium <dastelljes@outlook.com>
2024-11-12 18:58:59 -0600

    updated init.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+"""Initialize the API views"""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Importing each module to register routes
 from api.v1.views.index import *
-from api.v1.views.cities import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *  # New import for amenities
